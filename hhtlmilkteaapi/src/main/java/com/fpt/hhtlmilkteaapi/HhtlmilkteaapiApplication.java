012c62c16a6a10f47004d3e1d003209f12b3fc76
Huy PHẠM <toilahuy098@gmail.com>
2021-07-19 19:26:21 +0700

    first commit

---
+++ hhtlmilkteaapi/src/main/java/com/fpt/hhtlmilkteaapi/HhtlmilkteaapiApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.hhtlmilkteaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhtlmilkteaapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhtlmilkteaapiApplication.class, args);
+    }
+
+}
---
+++ hhtlmilkteaapi/src/test/java/com/fpt/hhtlmilkteaapi/HhtlmilkteaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.hhtlmilkteaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhtlmilkteaapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
