e2fdcc039efb1a5411c45b2efe1ac09b92503e30
Huy PHẠM <toilahuy098@gmail.com>
2021-09-03 14:13:36 +0700

    fix responsive

--- hhtlmilkteauser/src/components/ShoppingCart/index.jsx
+++ hhtlmilkteauser/src/components/ShoppingCart/index.jsx
@@ -24,8 +24,13 @@ const useStyles = makeStyles((theme) => ({
     width: "auto",
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-      width: 1000,
+    [theme.breakpoints.down("md")]: {
+      width: 800,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: 600,
       marginLeft: "auto",
       marginRight: "auto",
     },
